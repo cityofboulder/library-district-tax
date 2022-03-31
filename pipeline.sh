a24b1840be90c80a40d4f3fabb15bba5b5fcd473
Jesse Nestler <jesse.nestler@colorado.edu>
2022-03-31 13:33:29 -0600

    Alter paths to setup files

--- pipeline.sh
+++ pipeline.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # create the database
-psql -U postgres -f create_db.sql
+psql -U postgres -f ./setup/create_db.sql
 # initialize the database schema and extensions
-psql -U postgres -d library -f initialize_db.sql
+psql -U postgres -d library -f ./setup/initialize_db.sql
 # create raw and processing tables
-psql -U postgres -d library -f create_tables.sql
+psql -U postgres -d library -f ./setup/create_tables.sql
 # extract spatial data
-sh extract_spatial.sh
+sh ./setup/extract_spatial.sh
 # extract tabular data
-psql -d library -f extract_csv.sql
+psql -d library -f ./setup/extract_csv.sql
