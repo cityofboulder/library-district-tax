b5f98905d2057bc7c8a81152698bd98545b926f6
Jesse Nestler <jesse.nestler@colorado.edu>
2022-03-31 11:10:55 -0600

    Alter search path

--- initialize_db.sql
+++ initialize_db.sql
@@ -1,7 +1,26 @@
 -- Run this file as a superuser connected to the user's database of choice
-create schema if not exists sandbox;
-create schema if not exists raw;
-create schema if not exists proc;
-create schema if not exists clean;
-set search_path = sandbox, raw, proc, clean;
-create extension if not exists postgis;
+
+-- Drop everything
+DROP SCHEMA IF EXISTS SANDBOX CASCADE;
+
+DROP SCHEMA IF EXISTS RAW CASCADE;
+
+DROP SCHEMA IF EXISTS PROC CASCADE;
+
+DROP SCHEMA IF EXISTS CLEAN CASCADE;
+
+DROP EXTENSION IF EXISTS POSTGIS CASCADE;
+
+-- Create schema
+CREATE SCHEMA SANDBOX;
+
+CREATE SCHEMA RAW;
+
+CREATE SCHEMA PROC;
+
+CREATE SCHEMA CLEAN;
+
+SET SEARCH_PATH = PUBLIC, SANDBOX, RAW, PROC, CLEAN;
+
+-- Add postgis extension
+CREATE EXTENSION IF NOT EXISTS POSTGIS;
