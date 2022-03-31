8a698a41739475baa05a60ae5a10e825d4d1bc79
Jesse Nestler <jesse.nestler@colorado.edu>
2022-03-31 15:26:50 -0600

    Add a processing step

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Extract data
 # create the database
 psql -U postgres -f ./setup/create_db.sql
 # initialize the database schema and extensions
@@ -10,3 +11,7 @@ psql -U postgres -d library -f ./setup/create_tables.sql
 sh ./setup/extract_spatial.sh
 # extract tabular data
 psql -d library -f ./setup/extract_csv.sql
+
+# Process data
+# format raw into processing schema
+psql -U postgres -d library -f ./processing/format_raw_data.sql
