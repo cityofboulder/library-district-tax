f6dc4799ccbe7794efad07d384146ea3648b7253
Jesse Nestler <jesse.nestler@colorado.edu>
2022-03-31 10:10:13 -0600

    Rename file and add commands

---
+++ extract_spatial.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#---COUNTY DATA---
+# parcels
+echo "Extracting parcels..."
+ogr2ogr \
+-f PostgreSQL \
+Pg:"dbname=library" \
+GeoJSON:"https://opendata.arcgis.com/datasets/89ae49d4ddf246388ee5f5e952aa84db_0.geojson" \
+-nln "raw.parcels" \
+-t_srs "EPSG:2876" \
+-overwrite
+
+# municipalities
+echo "Extracting municipalities..."
+ogr2ogr \
+-f PostgreSQL \
+Pg:"dbname=library" \
+GeoJSON:"https://opendata.arcgis.com/datasets/9597d3916aba47e887ca563d5ac15938_0.geojson" \
+-nln "raw.municipalities" \
+-t_srs "EPSG:2876" \
+-overwrite
+
+# fire perimeters
+echo "Extracting fire perimeters..."
+ogr2ogr \
+-f PostgreSQL \
+Pg:"dbname=library" \
+GeoJSON:"https://opendata.arcgis.com/datasets/61f20f4a64274969a9e740eda5c62de7_0.geojson" \
+-nln "raw.fires" \
+-t_srs "EPSG:2876" \
+-overwrite
+
+#---BOULDER DATA---
+# library district
+echo "Extracting library districts..."
+ogr2ogr \
+-f PostgreSQL \
+Pg:"dbname=library" \
+GeoJSON:"https://opendata.arcgis.com/datasets/5f29b5fb39c74f5c99717da73c6c62cc_0.geojson" \
+-nln "raw.district" \
+-t_srs "EPSG:2876" \
+-overwrite
