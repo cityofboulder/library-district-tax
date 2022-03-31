7d0146a711f974654b5b98087080dd29f2380288
Jesse Nestler <jesse.nestler@colorado.edu>
2022-03-31 10:39:46 -0600

    Add schema

--- initialize_db.sql
+++ initialize_db.sql
@@ -1,6 +1,7 @@
 -- Run this file as a superuser connected to the user's database of choice
 create schema if not exists sandbox;
 create schema if not exists raw;
+create schema if not exists proc;
 create schema if not exists clean;
-set search_path = sandbox, raw, clean;
+set search_path = sandbox, raw, proc, clean;
 create extension if not exists postgis;
