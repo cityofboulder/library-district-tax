19653e1cdeeabb583d6271d95939a400a2a046b6
Jesse Nestler <jesse.nestler@colorado.edu>
2022-08-09 15:59:22 -0600

    Add library district field to retain all parcels

--- processing/spatial_joins.sql
+++ processing/spatial_joins.sql
@@ -1,26 +1,27 @@
 /*
 When joining spatial data, the following need to be considered:
 
-    1. We only care about parcels in the library district
+    1. Whether the parcel is in the library district
 	2. Which municipality the parcel resides in
 	3. Whether the parcel was impacted by the Marshall Fire
 	4. Which voting precinct the parcel resides in
 
-The library district is the only inner join we want; otherwise,
-we still want to retain parcels even if they do not have a
-spatial relationship with the comparing geometry; thus, left
-outer joins.
+We still want to retain parcels even if they do not have a
+spatial relationship with the library district or marshall fire; thus, 
+we use left outer joins and attribute the results based on those spatial
+tests.
 */
 
-INSERT INTO CLEAN.LIBRARY_PARCELS (OID, PARCELNUM, PRECINCT, MUNINAME, MARSHFIRE, GEOM)
+INSERT INTO CLEAN.LIBRARY_PARCELS (OID, PARCELNUM, PRECINCT, INLIBDIST, MUNINAME, MARSHFIRE, GEOM)
 SELECT P.OID,
 	P.PARCELNUM,
 	PC.PRECINCTNUM PRECINCT,
+	CASE WHEN L.OID IS NULL THEN FALSE ELSE TRUE END INLIBDIST,
 	CASE WHEN M.MUNINAME IS NULL THEN 'Unincorporated' ELSE M.MUNINAME END MUNINAME,
 	CASE WHEN F.FIRENAME IS NULL THEN FALSE ELSE TRUE END MARSHFIRE,
 	P.GEOM
 FROM PROC.PARCELS P
-INNER JOIN PROC.LIBRARY L
+LEFT JOIN PROC.LIBRARY L
 ON ST_WITHIN(ST_POINTONSURFACE(P.GEOM), L.GEOM)
 LEFT OUTER JOIN PROC.MUNICIPALITIES M
 ON ST_WITHIN(ST_POINTONSURFACE(P.GEOM), M.GEOM)
