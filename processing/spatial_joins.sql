f5f27f52c020f4bffa84cc53744ed68ea465f88f
Jesse Nestler <jesse.nestler@colorado.edu>
2022-08-10 10:48:46 -0600

    Dissolve polygons by parcelnum
    Requires more assumptions about the underlying geoms, see file header.
    Aggregations are performed on all columns other than parcelnum.

--- processing/spatial_joins.sql
+++ processing/spatial_joins.sql
@@ -2,9 +2,21 @@
 When joining spatial data, the following need to be considered:
 
     1. Whether the parcel is in the library district
+		a.	If a parcel number is associated with multiple polygons and any
+			one of those is inside the district, then all of them are
+			dissolved and labelled as inside the district
 	2. Which municipality the parcel resides in
+		a.	If a parcel number is associated with multiple polygons and each
+			falls in a different municipality, then all of them are
+			dissolved and labelled as part of both municipalities
 	3. Whether the parcel was impacted by the Marshall Fire
+		a.	If a parcel number is associated with multiple polygons and any
+		of those were affected by the marshall fire, then all of them are
+		dissolved and labelled as impacted by the fire.
 	4. Which voting precinct the parcel resides in
+		a.	If a parcel number is associated with multiple polygons and each
+			falls in a different voting district, then all of them are
+			dissolved and labelled as part of both voting districts
 
 We still want to retain parcels even if they do not have a
 spatial relationship with the library district or marshall fire; thus, 
@@ -14,11 +26,17 @@ tests.
 
 INSERT INTO CLEAN.LIBRARY_PARCELS (PARCELNUM, PRECINCT, INLIBDIST, MUNINAME, MARSHFIRE, GEOM)
 SELECT P.PARCELNUM,
-	PC.PRECINCTNUM PRECINCT,
-	CASE WHEN L.OID IS NULL THEN FALSE ELSE TRUE END INLIBDIST,
-	CASE WHEN M.MUNINAME IS NULL THEN 'Unincorporated' ELSE M.MUNINAME END MUNINAME,
-	CASE WHEN F.FIRENAME IS NULL THEN FALSE ELSE TRUE END MARSHFIRE,
-	P.GEOM
+	STRING_AGG(DISTINCT PC.PRECINCTNUM, ', ') PRECINCT,
+	CASE
+		WHEN SUM(CASE WHEN L.OID IS NOT NULL THEN 1 ELSE 0 END) > 0
+		THEN TRUE ELSE FALSE
+	END INLIBDIST,
+	STRING_AGG(DISTINCT CASE WHEN M.MUNINAME IS NULL THEN 'Unincorporated' ELSE M.MUNINAME END, ', ') MUNINAME,
+	CASE 
+		WHEN SUM(CASE WHEN F.FIRENAME IS NOT NULL THEN 1 ELSE 0 END) > 0
+	 	THEN TRUE ELSE FALSE
+	END MARSHFIRE,
+	ST_COLLECT(P.GEOM) GEOM
 FROM PROC.PARCELS P
 LEFT JOIN PROC.LIBRARY L
 ON ST_WITHIN(ST_POINTONSURFACE(P.GEOM), L.GEOM)
@@ -27,4 +45,5 @@ ON ST_WITHIN(ST_POINTONSURFACE(P.GEOM), M.GEOM)
 LEFT OUTER JOIN PROC.FIRES F
 ON ST_INTERSECTS(P.GEOM, F.GEOM)
 LEFT OUTER JOIN PROC.PRECINCTS PC
-ON ST_WITHIN(ST_POINTONSURFACE(P.GEOM), PC.GEOM);
+ON ST_WITHIN(ST_POINTONSURFACE(P.GEOM), PC.GEOM)
+GROUP BY PARCELNUM;
